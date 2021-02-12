ea84a9b3a86bc8ed542d05452a707e172860553a
tommibergman <tommi.bergman@iki.fi>
2021-02-12 16:04:59 +0200

    add settings sample

---
+++ code/settings-sample-safe.py
@@ -0,0 +1,36 @@
+global output_pdf_path
+global basepath
+global pngdpi
+global output_png_path
+global output_jpg_path
+global rawoutput
+global basepathraw
+global output
+global paper
+global EXPs
+global EXPS
+global EXP_NAMEs
+
+pngdpi=600
+basepath='tm5-soa/'
+# output_png_path=basepath+'figures/draft-v0.9/png/'
+# output_pdf_path=basepath+'figures/draft-v0.9/pdf/'
+# output_jpg_path=basepath+'figures/draft-v0.9/jpg/'
+output_png_path=basepath+'figures/submission/'
+output_pdf_path=basepath+'figures/submission/'
+output_jpg_path=basepath+'figures/submission/'
+rawoutput=basepath+'output/raw/'
+basepathraw=rawoutput
+output=basepath+'output/'
+basepathprocessed=output+'/processed/'
+paper=basepath+'paper/'
+EXPs=['newsoa-ri','oldsoa-bhn']
+EXPS=['newsoa-ri','oldsoa-bhn']
+EXP_NAMEs=['NEWSOA','OLDSOA']
+aeronet_path='obs-data/aeronet/'
+path_improve_col=basepathprocessed+'/improve_col2/'
+raw_store='/BIGDISK/'
+#paper=basepath+'paper/'
+
+# for added output
+debug=False
